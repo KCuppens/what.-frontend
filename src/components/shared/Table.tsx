17ef758625053884e1800c0e3031e4e0a4a992e8
Kobe Cuppens <kobecuppens@hotmail.com>
2023-11-09 11:18:42 +0100

    feat: search product, storing filters, store in session

--- src/components/shared/Table.tsx
+++ src/components/shared/Table.tsx
@@ -1,23 +1,58 @@
 import SearchForm from '../form/SearchForm';
 import { useProductContext } from '../../contexts/ProductContext';
 import ProductItem from './TableItem';
+import { useState } from 'react';
 
 const Table = () => {
-  const { products, isLoading } = useProductContext();
+  const { products, isLoading, setOrder } = useProductContext();
+  const initialSortField = localStorage.getItem('sortField') || '';
+  const initialSortDirection = localStorage.getItem('sortDirection') || '';
+
+  const [sortField, setSortField] = useState(initialSortField);
+  const [sortDirection, setSortDirection] = useState(initialSortDirection);
+  const handleSort = field => {
+    const isAscending = sortField === field && sortDirection === 'asc';
+    const newOrder = isAscending ? `-${field}` : field;
+    const newDirection = isAscending ? 'desc' : 'asc';
+
+    setOrder(newOrder);
+    setSortField(field);
+    setSortDirection(newDirection);
+
+    localStorage.setItem('sortField', field);
+    localStorage.setItem('sortDirection', newDirection);
+  };
+  const getArrowStyle = (field, direction) => {
+    if (field !== sortField) {
+      return 'text-gray-400'; // Default color for inactive fields
+    }
+    return sortDirection === direction ? 'text-blue-600' : 'text-gray-400';
+  };
   return (
     <div className="mx-auto max-w-screen-xl px-4 lg:px-12">
       <div className="bg-white relative shadow-md sm:rounded-lg overflow-hidden">
-        { isLoading && <div className="text-center">Loading...</div> }
+        {isLoading && <div className="text-center">Loading...</div>}
         <SearchForm />
         <div className="overflow-x-auto">
           <table className="w-full text-sm text-left text-gray-500 ">
             <thead className="text-xs text-gray-700 uppercase bg-gray-50">
               <tr>
-                <th scope="col" className="px-4 py-3">
-                  ID &#9650;&#9660;
+                <th
+                  scope="col"
+                  className="px-4 py-3"
+                  onClick={() => handleSort('id')}
+                >
+                  ID <span className={getArrowStyle('id', 'asc')}>&#9650;</span>
+                  <span className={getArrowStyle('id', 'desc')}>&#9660;</span>
                 </th>
-                <th scope="col" className="px-4 py-3">
-                  Name &#9650;&#9660;
+                <th
+                  scope="col"
+                  className="px-4 py-3"
+                  onClick={() => handleSort('name')}
+                >
+                  Name{' '}
+                  <span className={getArrowStyle('name', 'asc')}>&#9650;</span>
+                  <span className={getArrowStyle('name', 'desc')}>&#9660;</span>
                 </th>
                 <th scope="col" className="px-4 py-3">
                   Description
@@ -34,9 +69,9 @@ const Table = () => {
               </tr>
             </thead>
             <tbody>
-                {products.map(product => (
-                    <ProductItem key={product.id} product={product} />
-                ))}
+              {products.map(product => (
+                <ProductItem key={product.id} product={product} />
+              ))}
             </tbody>
           </table>
         </div>
--- src/contexts/ProductContext.jsx
+++ src/contexts/ProductContext.jsx
@@ -5,11 +5,24 @@ const ProductContext = createContext();
 export const useProductContext = () => useContext(ProductContext);
 
 export const ProductProvider = ({ children }) => {
+    const initialSearchQuery = localStorage.getItem('searchQuery') || '';
+    const initialOrder = localStorage.getItem('order') || 'name';
+
     const [products, setProducts] = useState([]);
+    const [searchQuery, setSearchQuery] = useState(initialSearchQuery);
+    const [order, setOrder] = useState(initialOrder);
     const [isLoading, setIsLoading] = useState(true);
     const apiUrl = import.meta.env.VITE_API_URL;
     useEffect(() => {
-        fetch(apiUrl + 'products/products/')
+        localStorage.setItem('searchQuery', searchQuery);
+        localStorage.setItem('order', order);
+
+        const queryParams = new URLSearchParams();
+        if (searchQuery) queryParams.append('search', searchQuery);
+        if (order) queryParams.append('ordering', order);
+        setIsLoading(true);
+        const url = `${apiUrl}products/products/?${queryParams.toString()}`;
+        fetch(url)
             .then(response => response.json())
             .then(data => {
                 setProducts(data);
@@ -19,10 +32,10 @@ export const ProductProvider = ({ children }) => {
                 console.error('Error fetching products:', error);
                 setIsLoading(false);
             });
-    }, []);
+    }, [searchQuery, order]);
 
     return (
-        <ProductContext.Provider value={{ products, isLoading }}>
+        <ProductContext.Provider value={{ products, isLoading, setSearchQuery, setOrder }}>
             {children}
         </ProductContext.Provider>
     );
