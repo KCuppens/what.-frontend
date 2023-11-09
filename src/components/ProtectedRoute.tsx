8d4ed4d28dc78e82f8695cc704d4a681a5cf73d1
Kobe Cuppens <kobecuppens@hotmail.com>
2023-11-09 08:40:15 +0100

    feat: setup project using docker and add basic styling

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,11 +1,16 @@
 import { Navigate } from 'react-router-dom';
+import { ReactNode } from 'react';
+
+interface ProtectedRouteProps {
+  children: ReactNode;
+}
 
 const isAuthenticated = () => {
   // Replace this with your actual logic to check authentication
   return true; // example placeholder
 };
 
-const ProtectedRoute = ({ children }) => {
+const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
   return isAuthenticated() ? children : <Navigate to="/" />;
 };
 
